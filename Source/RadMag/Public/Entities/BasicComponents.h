9d75efc77c3637b9c4916f76aa93849a255595ca
BlackDEK <dimoni546@gmail.com>
2020-12-20 16:31:34 +0500

    Move struct

--- Source/RadMag/Public/Entities/BasicComponents.h
+++ Source/RadMag/Public/Entities/BasicComponents.h
@@ -3,27 +3,10 @@
 #pragma once
 #include "CoreMinimal.h"
 #include "entt.hpp"
-#include "Containers/StaticArray.h"
 
 /** */
 struct FBasicData
 {
     entt::entity Id;
     FName Name;
-};
-
-/** */
-struct FHeadOfArray
-{
-    SIZE_T Children{0};
-    entt::entity First{entt::null};
-    entt::entity End{entt::null};
-};
-
-/** */
-struct FNode
-{    
-    entt::entity Parent{entt::null};
-    entt::entity Prev{entt::null};
-    entt::entity Next{entt::null};
 };
