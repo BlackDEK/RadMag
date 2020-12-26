9d533cb295cef02528571865848c2c941335b75a
Dima <dimoni546@gmail.com>
2020-12-26 09:26:58 +0500

    Update RadMag.Target.cs

--- Source/RadMag.Target.cs
+++ Source/RadMag.Target.cs
@@ -9,6 +9,6 @@ public class RadMagTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "RadMag", "ECSCore"} );
+		ExtraModuleNames.AddRange( new string[] { "RadMag"} );
 	}
 }
