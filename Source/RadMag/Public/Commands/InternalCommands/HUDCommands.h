4ba2e3d524b995431eede49e8e1b7dca444dbf74
Dima <dimoni546@gmail.com>
2020-12-28 19:22:18 +0500

    Add new commands

---
+++ Source/RadMag/Public/Commands/InternalCommands/HUDCommands.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+#include "DistrictCommands.h"
+#include "GameData.h"
+
+namespace HUDCommands
+{
+	inline FText WorldInfoGetter(UGameData* GameData)
+	{
+		//const auto Pair = BasicInternalCommands::Get<FGameRules>(GameData);
+		//if(Pair.Key == false) return FText();
+		//const auto MapRules = Pair.Value.MapRules;	
+		FTextBuilder TextBuilder;
+		TextBuilder.AppendLine(FString("Entities: " + FString::FromInt(GameData->World.alive())));
+		return TextBuilder.ToText();
+	}
+
+	inline FText DistrictInfoGetter(const FVector& Location, UGameData* GameData)
+	{
+		const auto Pair  = DistrictCommands::GetDistrict(Location, GameData);
+		if(Pair.Key == false) return FText();
+		const auto District = Pair.Value;	
+		FTextBuilder TextBuilder;
+		TextBuilder.AppendLine(FString("Id: " + FString::FromInt(static_cast<uint32>(District.BasicData.Id))));
+		TextBuilder.AppendLine(FString("Name: " + District.BasicData.Name.ToString()));
+		TextBuilder.AppendLine(FString(TEXT("CubeCoordinate: ") + District.CubeCoordinate.ToString()));		
+		return TextBuilder.ToText();
+	}
+
+	inline FText MapInfoGetter(UGameData* GameData)
+	{
+		const auto Pair = BasicInternalCommands::Get<FGameRules>(GameData);
+		if(Pair.Key == false) return FText();
+		const auto MapRules = Pair.Value.MapRules;	
+		FTextBuilder TextBuilder;
+		TextBuilder.AppendLine(FString("Length: " + FString::FromInt(MapRules.ChunkCountOX * MapRules.ChunkSize)));
+		TextBuilder.AppendLine(FString("Height: " + FString::FromInt(MapRules.ChunkCountOY * MapRules.ChunkSize)));
+		TextBuilder.AppendLine(FString(TEXT("ChunkSize: ") + FString::FromInt(MapRules.ChunkSize)));
+		return TextBuilder.ToText();
+	}
+}
---
+++ Source/RadMag/Public/Commands/InternalCommands/RenderCommands.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameData.h"
+#include "HexMetricsCommands.h"
+#include "RenderCommandsContexts.h"
+#include "Commands/ExternalCommands/BasicExternalCommands.h"
+#include "Entities/District.h"
+
+namespace RenderCommands
+{
+	inline void GenerateChunksCache(TArray<FChunkCache>& Caches, UGameData* GameData)
+	{
+		const auto Pair = BasicInternalCommands::Get<FGameRules>(GameData);
+		if(Pair.Key == false) return;
+		const auto GameRules = Pair.Value;
+		TArray<FDistrict> Districts;
+		BasicInternalCommands::GetAll(Districts, GameData);
+		Caches.Init(FChunkCache(), GameRules.MapRules.ChunkCountOX * GameRules.MapRules.ChunkCountOY);
+		
+		for (decltype(auto) District : Districts)
+		{
+			const auto OffsetCoordinate = HexMetricsCommands::ConvertToOffsetCoordinate(District.CubeCoordinate);
+			const auto ChunkId = HexMetricsCommands::ConvertToChunkIndex(OffsetCoordinate, GameData);
+			const FVector Center = HexMetricsCommands::ConvertToRealCoordinate(OffsetCoordinate, GameData);
+			for (int32 Index = 0; Index < 6; Index++)
+			{
+				int32 VertexIndex = Caches[ChunkId].Vertices.Num();
+				Caches[ChunkId].Vertices.Add(Center);
+				Caches[ChunkId].Vertices.Add(Center + GameRules.RenderRules.Corners[Index]);
+				Caches[ChunkId].Vertices.Add(Center + GameRules.RenderRules.Corners[Index + 1]);
+				Caches[ChunkId].Triangles.Add(VertexIndex);
+				Caches[ChunkId].Triangles.Add(VertexIndex + 1);
+				Caches[ChunkId].Triangles.Add(VertexIndex + 2);
+			}
+			
+		}
+	}
+}
