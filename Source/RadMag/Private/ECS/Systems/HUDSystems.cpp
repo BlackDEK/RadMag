0213973af34019e198aa801160acd8ccd4e85358
Dima <dimoni546@gmail.com>
2021-01-28 23:58:17 +0500

    Add factory for all systems witch work with HUD and add System CityInfoGetter

---
+++ Source/RadMag/Private/ECS/Systems/HUDSystems.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ECS/Systems/HUDSystems.h"
+#include "ECS/GameData.h"
+#include "ECS/Commands/HUDCommands.h"
+#include "ECS/Systems/BasicSystemFactory.h"
+
+USystem* UHUDSystems::MakeSystemCityInfoGetter
+(TScriptInterface<IAbstractWidget> Widget, const FVector& Location, UObject* Outer)
+{
+	const auto CityInfoGetter = [Widget, Location](UGameData* GameData)
+	{
+		auto& World = GameData->World;
+		const auto Text = Commands::CityInfoGetter(Location, World);
+		IAbstractWidget::Execute_OnUpdateInfo(Widget.GetObject(), Text);
+	};
+	return UBasicSystemFactory::CreateSystem(CityInfoGetter, Outer);
+}
