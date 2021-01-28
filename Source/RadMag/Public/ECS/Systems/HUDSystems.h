cbfe0a01d951fe4712e773d05fde5123c515db16
Dima <dimoni546@gmail.com>
2021-01-28 23:54:03 +0500

    Add factory for all systems witch work with HUD

---
+++ Source/RadMag/Public/ECS/Systems/HUDSystems.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Interfaces/AbstractWidget.h"
+#include "UObject/Object.h"
+#include "HUDSystems.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RADMAG_API UHUDSystems final : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	static USystem* MakeSystemCityInfoGetter
+	(TScriptInterface<IAbstractWidget> Widget, const FVector& Location, UObject* Outer);
+};
